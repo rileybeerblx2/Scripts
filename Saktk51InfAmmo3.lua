9155e7d50ac6622d2bcbcdf90cf70fc9f03a2a4b
rileybeerblx2 <162878813+rileybeerblx2@users.noreply.github.com>
2024-04-07 23:09:36 +0800

    Create Saktk51InfAmmo3.lua

---
+++ Saktk51InfAmmo3.lua
@@ -0,0 +1,11 @@
+local function ChangeAmmoValue()
+    for i, v in pairs(getgc()) do
+        if type(v) == "function" and islclosure(v) and not is_synapse_function(v) then
+            local x = debug.getconstants(v)
+            if table.find(x, "ammo") and table.find(x, "update_ammo_gui") and table.find(x, "has_scope") then
+                debug.setconstant(v, 2, -999999)
+            end
+        end
+    end
+end
+ChangeAmmoValue()
