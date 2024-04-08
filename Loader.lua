71162a208191c013c4ef7ddddc2c1fe7fbf86206
rileybeerblx2 <162878813+rileybeerblx2@users.noreply.github.com>
2024-04-08 22:35:00 +0800

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,8 @@
+return {
+    ['Apeirophobia'] = 10277607801,
+    ['ForgottenMemories'] = 8482713490,
+    ['RaiseAYippie'] = 9742783208,
+    ['SlapBattles'] = 6403373529,
+    ['Arsenal'] = 286090429,
+    ['Evade'] = 9872472334,
+}
