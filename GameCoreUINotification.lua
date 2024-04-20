0851b07a5ebcc72df71768e138b98467c34be4c1
rileybeerblx2 <162878813+rileybeerblx2@users.noreply.github.com>
2024-04-20 16:58:44 +0800

    Create GameCoreUINotification.lua

---
+++ GameCoreUINotification.lua
@@ -0,0 +1,7 @@
+game.StarterGui:SetCore("SendNotification", {
+			Title = "Testing";
+			Text = "Notification Testing";
+			Duration = "10";
+			Button1 = "Close Notification";
+			Callback = Bindable
+		})
